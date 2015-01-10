b772ab6e221ff057bf6c13a40dab68674cc6cad7
Jacek Nosal <jacek.nosal@outlook.com>
2015-01-10 21:14:33 +0100

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'seth',
+    packages = ['seth'],
+    version = '0.1',
+    description = 'Smart and practical set of utilities for Pyramid framework',
+    author = 'Jacek Nosal',
+    author_email = 'jacek.nosal@outlook.com',
+    url = 'https://github.com/jnosal/seth',
+    keywords = ['sqlalchemy', 'pyramid'],
+    classifiers = [],
+)
