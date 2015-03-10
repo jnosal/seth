b636d15a5c60eaf7bf326430ee29b2d00d8b0f38
Jacek Nosal <nosaljacek@gmail.com>
2015-03-10 16:34:20 +0100

    updated batman class

--- examples/chat/custom.py
+++ examples/chat/custom.py
@@ -10,3 +10,15 @@ class Batman(DefaultSethApplication,
     def __init__(self, **kwargs):
         super(Batman, self).__init__(**kwargs)
         logging.critical("I'm Batman. BEHOLD")
+
+    def add_subscriber(self, channel, subscriber):
+        logging.critical("Batman has intercepted your subscribtion !")
+        super(Batman, self).add_subscriber(channel, subscriber)
+
+    def remove_subscriber(self, channel, subscriber):
+        logging.critical("Batman has intercepted your unsubscribtion !")
+        super(Batman, self).add_subscriber(channel, subscriber)
+
+    def broadcast(self, message, channel=None, sender=None):
+        logging.critical("Batman has intercepted broadcast message !")
+        super(Batman, self).broadcast(message, channel, sender)
